9e9ddd01e100edc1f7d6318954f1bab88fe40fe1
nknk567 <noah.kubli@uzh.ch>
2023-07-20 16:20:13 +0200

    disabled Grackle

--- main/src/propagator/std_cloud.hpp
+++ main/src/propagator/std_cloud.hpp
@@ -260,7 +260,7 @@ public:
         timer.step("Timestep");
 
 //#pragma omp parallel for schedule(static)
-        for (size_t i = first; i < last; i++)
+        /*for (size_t i = first; i < last; i++)
         {
             bool haveMui = !d.mui.empty();
             T    cv      = idealGasCv(haveMui ? d.mui[i] : d.muiConst, d.gamma);
@@ -284,7 +284,7 @@ public:
             const T du = (u_cool - u_old) / d.minDt;
             d.du[i] += du;
         }
-        timer.step("GRACKLE chemistry and cooling");
+        timer.step("GRACKLE chemistry and cooling");*/
 
         computePositions(first, last, d, domain.box());
         timer.step("UpdateQuantities");

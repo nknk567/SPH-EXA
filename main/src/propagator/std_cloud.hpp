aa0b762e5dc5bc1fa59a3df2d3e177775aa8cd90
nknk567 <noah.kubli@uzh.ch>
2023-07-20 17:14:57 +0200

    changed cloud prop

--- main/src/propagator/std_cloud.hpp
+++ main/src/propagator/std_cloud.hpp
@@ -211,7 +211,7 @@ public:
             computeTimestep(first, last, d);
             timer.step("Timestep");
             // Friction
-            const T friction_time{2. * d.minDt};
+            const T friction_time{10. * d.minDt};
             T       max_fric{0.};
             T       fric_tot{0.};
             for (size_t i = first; i < last; i++)
@@ -227,7 +227,7 @@ public:
                 fric_tot += std::abs(fric_x) + std::abs(fric_y) + std::abs(fric_z);
             }
             fric_tot /= d.numParticlesGlobal;
-            if (step > 100 && fric_tot < 0.01) break;
+            if (step > 100 && fric_tot < 0.000) break;
             std::cout << "fric_tot " << fric_tot << std::endl;
             computePositions(first, last, d, domain.box());
             timer.step("UpdateQuantities");

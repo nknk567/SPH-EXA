aaf1bac477515a8629e2f1d2fc757c988f14692b
nknk567 <noah.kubli@uzh.ch>
2023-08-10 11:13:59 +0200

    x

--- main/src/init/cloud_init.hpp
+++ main/src/init/cloud_init.hpp
@@ -232,7 +232,7 @@ public:
             return ret;
         };
 
-       // transferToDevice(d, 0, d.x.size(), bl());
+        // transferToDevice(d, 0, d.x.size(), bl());
 
         domain.syncGrav(get<"keys">(d), get<"x">(d), get<"y">(d), get<"z">(d), get<"h">(d), get<"m">(d),
                         std::tuple_cat(get<ConservedFields>(d), get<CoolingFields>(simData.chem)),
@@ -278,18 +278,21 @@ public:
             }
         };
 
-
-
         auto calculateChemistry = [&, &chem = simData.chem]()
         {
-            const auto oldFields = chem.fields;
-            T          max_diff  = 0.;
+            // const auto oldFields = chem.fields;
+            T max_diff = 0.;
 
-//#pragma omp parallel for schedule(static) reduction(max : max_diff)
+            // #pragma omp parallel for schedule(static) reduction(max : max_diff)
             for (size_t i = first; i < last; ++i)
             {
+                std::array<T, chem.numFields> old;
+                for (size_t j = 0; j < chem.fields.size(); j++)
+                {
+                    old[j] = chem.fields[j][i];
+                }
                 T u = d.u[i];
-                std::cout << "rho: " << d.rho[i] << "u " << u << std::endl;
+                std::cout << "rho: " << d.rho[i] << "u " << u << "hi " << get<"HI_fraction">(chem)[i] << std::endl;
                 cooling_data.cool_particle(
                     0.01, d.rho[i], u, get<"HI_fraction">(chem)[i], get<"HII_fraction">(chem)[i],
                     get<"HM_fraction">(chem)[i], get<"HeI_fraction">(chem)[i], get<"HeII_fraction">(chem)[i],
@@ -302,7 +305,7 @@ public:
                     get<"H2_self_shielding_length">(chem)[i]);
                 for (size_t j = 0; j < chem.fields.size(); j++)
                 {
-                    const T diff = std::abs(chem.fields[j][i] - oldFields[j][i]);
+                    const T diff = std::abs(chem.fields[j][i] - old[j]);
                     max_diff     = std::max(max_diff, diff);
                 }
             }
@@ -313,9 +316,9 @@ public:
         while (true)
         {
             calculatePressure();
-            bool good =  (initDependent(simData));
-            //const T max_diff = calculateChemistry();
-            const T max_diff=0.;
+            bool good = (initDependent(simData));
+            // const T max_diff = calculateChemistry();
+            const T max_diff = 0.;
             n_it++;
             std::cout << "equilibrated " << n_it << "\t" << max_diff << std::endl;
             if (max_diff < 1e-6 && good) break;

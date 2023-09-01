3b00aa56b59f7d2739b68f03ca89741c33bbc050
nknk567 <noah.kubli@uzh.ch>
2023-09-01 14:40:43 +0200

    x

--- main/src/propagator/ve_grackle.hpp
+++ main/src/propagator/ve_grackle.hpp
@@ -124,9 +124,10 @@ public:
         }
         else
         {
-            domain.sync(get<"keys">(d), get<"x">(d), get<"y">(d), get<"z">(d), get<"h">(d), get<"m">(d),
-                            std::tuple_cat(get<ConservedFields>(d), get<CoolingFields>(simData.chem)),
-                            get<DependentFields>(d));
+            domain.sync(
+                get<"keys">(d), get<"x">(d), get<"y">(d), get<"z">(d), get<"h">(d),
+                std::tuple_cat(std::tie(get<"m">(d)), get<ConservedFields>(d), get<CoolingFields>(simData.chem)),
+                get<DependentFields>(d));
         }
         d.treeView = domain.octreeProperties();
     }

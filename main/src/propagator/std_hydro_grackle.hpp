8189facd7685cddd1345237092cab96ff01aa538
nknk567 <noah.kubli@uzh.ch>
2023-09-05 09:59:38 +0200

    x

--- main/src/propagator/std_hydro_grackle.hpp
+++ main/src/propagator/std_hydro_grackle.hpp
@@ -164,7 +164,7 @@ public:
         resizeNeighbors(d, domain.nParticles() * d.ngmax);
         findNeighborsSfc(first, last, d, domain.box());
         timer.step("FindNeighbors");
-        fill(get<"soft">(d), 0, domain.nParticlesWithHalos(), 0.02);
+        fill(get<"soft">(d), 0, domain.nParticlesWithHalos(), 0.01);
         fill(get<"phi">(d), 0, domain.nParticlesWithHalos(), 0.);
 
         computeDensity(first, last, d, domain.box());

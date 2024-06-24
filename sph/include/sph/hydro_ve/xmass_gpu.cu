b8e68da82f4c4d6e6c32622498de213400b57525
nknk567 <noah.kubli@uzh.ch>
2024-06-25 00:36:42 +0200

    split init

--- sph/include/sph/hydro_ve/xmass_gpu.cu
+++ sph/include/sph/hydro_ve/xmass_gpu.cu
@@ -90,8 +90,8 @@ __global__ void xmassGpu(Tc K, unsigned ng0, unsigned ngmax, const cstone::Box<T
         for (int ncIt = 0; ncIt <= ncMaxIteration; ++ncIt)
         {
             // bool tooMany   = (ncSph - 1) > ngmax;
-            bool notEnough = ncSph < ng0 - 5; //! unsigned!
-            bool tooMany   = (ncSph) > ng0 + 5;
+            bool notEnough = ncSph < ng0 - 10; //! unsigned!
+            bool tooMany   = (ncSph) > ng0 + 10;
             bool repeat    = (notEnough || tooMany) && i < bodyEnd;
 
             h_upper = tooMany ? h[i] : h_upper;
@@ -112,7 +112,7 @@ __global__ void xmassGpu(Tc K, unsigned ng0, unsigned ngmax, const cstone::Box<T
 
             ncSph =
                 1 + traverseNeighbors(bodyBegin, bodyEnd, x, y, z, h, tree, box, neighborsWarp, ngmax, globalPool)[0];
-            if (ncIt == ncMaxIteration && (ncSph < ng0 - 5 || ncSph > ng0 + 5)) { nc_h_convergenceFailure = true; }
+            if (ncIt == ncMaxIteration && (ncSph < ng0 - 10 || ncSph > ng0 + 10)) { nc_h_convergenceFailure = true; }
         }
 
         if (i >= bodyEnd) continue;

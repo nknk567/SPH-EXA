2b51ed396e3b7f1c556e72da4f50ba92982ddc37
nknk567 <noah.kubli@uzh.ch>
2024-08-14 11:02:55 +0200

    Measure accreted mass/mom.

--- physics/Planet/include/accretion_gpu.cu
+++ physics/Planet/include/accretion_gpu.cu
@@ -97,8 +97,8 @@ struct debug_zero
 template<typename T1, typename Th, typename Tremove, typename T2, typename Tm, typename Tv>
 void computeAccretionConditionGPU(size_t first, size_t last, const T1* x, const T1* y, const T1* z, const Th* h,
                                   Tremove* remove, const Tm* m, const Tv* vx, const Tv* vy, const Tv* vz,
-                                  const T2* spos, T2 star_size, T2 removal_limit_h, Tm& m_accr, Tv& vx_accr,
-                                  Tv& vy_accr, Tv& vz_accr)
+                                  const T2* spos, T2 star_size, T2 removal_limit_h, T2& m_accr, T2& vx_accr,
+                                  T2& vy_accr, T2& vz_accr)
 {
     cstone::LocalIndex numParticles = last - first;
     unsigned           numThreads   = 256;
